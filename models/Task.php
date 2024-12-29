813ec8edaaa3779986c69a12c3b520af8c529e1b
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-29 19:13:22 +0100

    ajouter class task in task.php

---
+++ models/Task.php
@@ -0,0 +1,48 @@
+<?php
+class Task {
+    private $conn;
+    private $table_name = "tasks";
+    private $usertask_table = "usertasks"; 
+
+    public $id;
+    public $title;
+    public $description;
+    public $status;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    
+    
+   
+    
+   
+  
+  
+
+   
+
+    
+   
+  
+    
+    
+    
+  
+
+   
+
+   
+  
+   
+    
+  
+
+   
+
+      
+     
+    
+}
+?>
