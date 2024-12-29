af3d9cd7a2f4d7b6ab6c6b109b52444dd1e8d572
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-29 19:10:37 +0100

    ajouter function getTaskTags

--- models/Tag.php
+++ models/Tag.php
@@ -12,7 +12,17 @@ class Tag {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     
-   
+    public function getTaskTags($taskId) {
+        $stmt = $this->conn->prepare("
+            SELECT t.* 
+            FROM Tags t 
+            JOIN TagsTasks tt ON t.id_tags = tt.tags_id 
+            WHERE tt.tasks_id = :taskId
+        ");
+        $stmt->bindParam(':taskId', $taskId);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 ?>
 
